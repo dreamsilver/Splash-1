97abdecfcd222ea45a15ef7b313010323d9ea7e8
sliver <sliver@board.com>
2018-01-29 10:58:04 +0800

    unity启动界面的设置1

---
+++ Assets/Script/GameGlobal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGlobal : MonoBehaviour 
+{   
+
+	// Use this for initialization
+	void Start () 
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            SDKMgr.HideSplash();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
---
+++ Assets/Script/SDKMgr.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class SDKMgr
+{
+    public static void HideSplash()
+    {
+        AndroidJavaClass androidClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject mainActivity = androidClass.GetStatic<AndroidJavaObject>("currentActivity");
+        mainActivity.Call("HideSplash");
+
+    }
+}
